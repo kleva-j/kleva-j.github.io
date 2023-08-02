88c1849b96a9e12d10307beae817c4ad1e8ba44a
kleva-j <kasmickleva@gmail.com>
2023-08-02 09:53:27 +0100

    chore: setup tailwindcss with postcss and autoprefixer

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import './App.css';
-
 import { useState } from 'react';
 
 import logo from '@/assets/logo.svg';
@@ -8,9 +6,9 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="text-center">
+      <header className="App-header min-h-screen flex bg-[#282c34] flex-col items-center justify-center text-white">
+        <img src={logo} className="h-[40vmin] pointer-events-none animate-spin-slow" alt="logo" />
         <p>Hello Vite + React!</p>
         <p>
           <button type="button" onClick={() => setCount((count) => count + 1)}>
@@ -21,12 +19,12 @@ function App() {
           Edit <code>App.tsx</code> and save to test HMR updates.
         </p>
         <p>
-          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          <a className="text-[#61dafb]" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
             Learn React
           </a>
           {' | '}
           <a
-            className="App-link"
+            className="text-[#61dafb]"
             href="https://vitejs.dev/guide/features.html"
             target="_blank"
             rel="noopener noreferrer"
